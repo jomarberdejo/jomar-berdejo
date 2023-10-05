8413dafc6e1296d8490f5b8c01395c54aae83a47
jomarberdejo <berdejojomar@gmail.com>
2023-10-06 00:23:18 +0800

    changed contact bg

--- src/js/observer.js
+++ src/js/observer.js
@@ -1,18 +1,18 @@
 const fadeElements = document.querySelectorAll('.fade-in, .fade-out');
 
-
 const options = {
   root: null,
   rootMargin: '0px',
-  threshold: 0.2, 
+  threshold: 0.2,
 };
-
 
 const callback = (entries, observer) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add('visible');
       entry.target.classList.remove('hidden');
+      
+      observer.unobserve(entry.target);
     } else {
       entry.target.classList.add('hidden');
       entry.target.classList.remove('visible');
@@ -20,11 +20,8 @@ const callback = (entries, observer) => {
   });
 };
 
-
 const observer = new IntersectionObserver(callback, options);
-
 
 fadeElements.forEach((element) => {
   observer.observe(element);
 });
-      
