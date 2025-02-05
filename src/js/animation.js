320f056788d1d293a766f35a79bfd9a93aefbaf9
jomar <berdejojomar@gmail.com>
2025-02-05 20:06:19 +0800

    updated the portfolio projects and skills

--- src/js/animation.js
+++ src/js/animation.js
@@ -10,17 +10,17 @@ document.addEventListener("DOMContentLoaded", function () {
     ease: "power3.out",
   });
 
-  const introText = document.querySelector(".intro h2");
+  // const introText = document.querySelector(".intro h2");
   const subTitle = document.querySelector(".intro h3");
   const introDescription = document.querySelector(".intro p");
-  const moreAboutBtn = document.querySelector(".more-about-btn");
+  // const moreAboutBtn = document.querySelector(".more-about-btn");
 
-  gsap.from(introText, {
-    opacity: 0,
-    y: 20,
-    duration: 1.5,
-    ease: "power3.out",
-  });
+  // gsap.from(introText, {
+  //   opacity: 0,
+  //   y: 20,
+  //   duration: 1.5,
+  //   ease: "power3.out",
+  // });
 
   gsap.from(subTitle, {
     opacity: 0,
@@ -38,12 +38,11 @@ document.addEventListener("DOMContentLoaded", function () {
     ease: "power3.out",
   });
 
-  gsap.from(moreAboutBtn, {
-    opacity: 0,
-    y: 20,
-    duration: 1.5,
-    delay: 0.9,
-    ease: "power3.out",
-  });
-  
-})
+  // gsap.from(moreAboutBtn, {
+  //   opacity: 0,
+  //   y: 20,
+  //   duration: 1.5,
+  //   delay: 0.9,
+  //   ease: "power3.out",
+  // });
+});
